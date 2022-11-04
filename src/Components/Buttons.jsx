d3f4573fc19ce2fbf3237d0147b20b609b4c412c
Jetigeri <szabo.gergo87j@gmail.com>
2022-11-04 19:58:44 +0100

    implemented button switchup feature

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -1,18 +1,23 @@
-import React from "react";
-import {motion} from "framer-motion"
+import React, {useState} from "react";
+import { motion } from "framer-motion";
 
-function Buttons({setLikeAction, likeAction}) {
+function Buttons({ setLikeAction, likeAction }) {
+  const [reverseButtons, setReverseButtons] = useState(false)
+
+
   return (
-      <div className="flex justify-around">
-    <motion.button className="w-20 h-20 bg-white rounded-full flex justify-center items-center bg-gradient-to-br from-rose-400 to-rose-700 border-2 drop-shadow-md"
-       whileHover={{ scale: 1.1 }}
-       transition={{
-           duration: 0.1,
-           type: "spring",
-           damping: 25,
-           stiffness: 500}
-       }
-       whileTap={{ scale: 0.9}}>
+    <div className={`flex flex-row${reverseButtons && "-reverse"}  justify-around`}>
+      <motion.button
+        className="w-20 h-20 bg-white rounded-full flex justify-center items-center bg-gradient-to-br from-rose-400 to-rose-700 border-2 drop-shadow-md"
+        whileHover={{ scale: 1.1 }}
+        transition={{
+          duration: 0.1,
+          type: "spring",
+          damping: 25,
+          stiffness: 500,
+        }}
+        whileTap={{ scale: 0.9 }}
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -20,11 +25,27 @@ function Buttons({setLikeAction, likeAction}) {
           strokeWidth="1.5"
           stroke="white"
           className="w-8 h-8"
-
-          onClick={() => setLikeAction({...likeAction, dislikePressed: !likeAction.dislikePressed})}
-          onMouseEnter={() => setLikeAction({...likeAction, dislikeHovered: !likeAction.dislikeHovered})}
-          onMouseLeave={() => setLikeAction({...likeAction, dislikeHovered: !likeAction.dislikeHovered})}
-        
+          onClick={() =>
+            setLikeAction({
+              ...likeAction,
+              dislikePressed: !likeAction.dislikePressed,
+            })
+          }
+          onMouseEnter={() =>{
+            setLikeAction({
+              ...likeAction,
+              dislikeHovered: !likeAction.dislikeHovered,
+            })
+            setReverseButtons(!reverseButtons)
+            }
+          }
+          onMouseLeave={() =>
+            setLikeAction({
+              ...likeAction,
+              dislikeHovered: !likeAction.dislikeHovered,
+            })
+          }
+    
         >
           <path
             strokeLinecap="round"
@@ -32,21 +53,19 @@ function Buttons({setLikeAction, likeAction}) {
             d="M6 18L18 6M6 6l12 12"
           />
         </svg>
-
-        
       </motion.button>
 
-
-
-      <motion.button className="w-20 h-20 bg-white rounded-full flex justify-center items-center bg-gradient-to-br from-green-400 to-green-700 border-2 drop-shadow-md"
-       whileHover={{ scale: 1.1 }}
-       transition={{
-           duration: 0.1,
-           type: "spring",
-           damping: 25,
-           stiffness: 500}
-       }
-       whileTap={{ scale: 0.9}}>
+      <motion.button
+        className="w-20 h-20 bg-white rounded-full flex justify-center items-center bg-gradient-to-br from-green-400 to-green-700 border-2 drop-shadow-md"
+        whileHover={{ scale: 1.1 }}
+        transition={{
+          duration: 0.1,
+          type: "spring",
+          damping: 25,
+          stiffness: 500,
+        }}
+        whileTap={{ scale: 0.9 }}
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -54,10 +73,24 @@ function Buttons({setLikeAction, likeAction}) {
           strokeWidth="1.5"
           stroke="white"
           className="w-8 h-8"
-
-          onClick={() => setLikeAction({...likeAction, likePressed: !likeAction.likePressed})}
-          onMouseEnter={() => setLikeAction({...likeAction, likeHovered: !likeAction.likeHovered})}
-          onMouseLeave={() => setLikeAction({...likeAction, likeHovered: !likeAction.likeHovered})}
+          onClick={() =>
+            setLikeAction({
+              ...likeAction,
+              likePressed: !likeAction.likePressed,
+            })
+          }
+          onMouseEnter={() =>
+            setLikeAction({
+              ...likeAction,
+              likeHovered: !likeAction.likeHovered,
+            })
+          }
+          onMouseLeave={() =>
+            setLikeAction({
+              ...likeAction,
+              likeHovered: !likeAction.likeHovered,
+            })
+          }
         >
           <path
             strokeLinecap="round"
@@ -66,8 +99,6 @@ function Buttons({setLikeAction, likeAction}) {
           />
         </svg>
       </motion.button>
-      
-  
     </div>
   );
 }
